a124cd6c8fb62c368082efa071863ef730320d52
Mighty-Chewbacca <alexmalcolm93@live.co.uk>
2014-09-25 15:48:04 +0100

    Up to and including stage 4 complete

---
+++ GP2Labs-Amalco200/GP2Labs-Amalco200/main.cpp
@@ -0,0 +1,21 @@
+//Header files
+#include <iostream>
+
+//Global variables here
+
+//Global functions
+void InitWindow(int width, int height, bool fullscreen)
+{
+
+}
+
+void CleanUp()
+{
+
+}
+
+//Main method - program entry point
+int main(int argc, char*arg[])
+{
+	return 0;
+}
