108931e1ea2d5c4c78921857d8ce134fb24c73d8
Mighty-Chewbacca <alexmalcolm93@live.co.uk>
2014-09-25 16:04:46 +0100

    Stage 5 completed
    created window which automatically closes, adding while loop next

--- GP2Labs-Amalco200/GP2Labs-Amalco200/main.cpp
+++ GP2Labs-Amalco200/GP2Labs-Amalco200/main.cpp
@@ -1,21 +1,49 @@
 //Header files
 #include <iostream>
 
+//header for sdl2 functionality
+#include <SDL.h>
+
 //Global variables here
+//pointer to SDL windows
+SDL_Window*window;
+
+//constants to control window creation
+const int WINDOW_HEIGHT = 480;
+const int WINDOW_WIDTH = 640;
 
 //Global functions
 void InitWindow(int width, int height, bool fullscreen)
 {
-
+	//create window
+	window = SDL_CreateWindow(
+		"Lab 1",					//Window title
+		SDL_WINDOWPOS_CENTERED,		//x pos centered
+		SDL_WINDOWPOS_CENTERED,		//y pos centered
+		width,						//width in pixels
+		height,						//height in pixels
+		SDL_WINDOW_OPENGL			//flags
+		);
 }
 
+//used to clean up once we exit app
 void CleanUp()
 {
-
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 }
 
 //Main method - program entry point
 int main(int argc, char*arg[])
 {
+	//init everything -SDL, if it is not zero we have a problem
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		std::cout << "ERROR SDL_Init" << SDL_GetError() << std::endl;
+		return-1;
+	}
+
+	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false);
+	CleanUp();
 	return 0;
 }
